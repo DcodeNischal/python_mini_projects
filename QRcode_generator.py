17e620be0a853ccc279571158420c58d44e2a034
DcodeNischal <dnischal11@gmail.com>
2022-06-15 18:32:52 +0545

    GodZer0

---
+++ QRcode_generator.py
@@ -0,0 +1,19 @@
+import qrcode #qrcode library
+
+value=input("Enter the value to be encoded in QRcode: ")
+#Taking the value to be encoded in QRcode
+
+img=qrcode.make(value) 
+#Creating the QRcode
+
+folder=input("Enter the folder full address where the QRcode will be stored: ")
+name=input("Enter the name of the file: ")
+ext=input("Enter the extension of the file: ")
+#Taking the name and extension of the file
+
+img.save(f"{folder}{name}.{ext}")
+#Saving the QRcode in the specified location
+#/home/dnischal11/Downloads/ is the path where the QRcode is saved.
+#you can change the path according to your system.
+
+print(f"QRcode is generated and saved in the location : {folder}{name}.{ext}")

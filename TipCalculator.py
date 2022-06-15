f173d3eb62b426e66523ddae11f40125fc9ecac1
DcodeNischal <dnischal11@gmail.com>
2022-06-15 17:46:34 +0545

    GodZer0

---
+++ TipCalculator.py
@@ -0,0 +1,14 @@
+#Inputs for the program:
+amount = float(input("Enter the amount of the bill : ").strip('$'))
+tip= int(input("Enter the tip percentage : ").strip('%'))
+tax= int(input("Enter the tax percentage : ").strip('%'))
+
+#Calculate the tip and tax
+tax_amount = amount * tax/100
+tip_amount= amount * tip/100
+#Calculate the total amount
+total= amount + tax_amount + tip_amount
+#Print the tip, tax and total amount
+print("The tip amount is : $ ", tip_amount)
+print("The tax amount is : $ ", tax_amount)
+print("The total amount including tax and tip is : $ ", total)
---
+++ timer.py
@@ -0,0 +1,23 @@
+import time # Import the time module
+
+
+def countdown(n):
+    while n:
+        mins, secs = divmod(n, 60) 
+        #divmod function divides the number in two parts.
+        timer = f'{mins:02d}:{secs:02d}'
+        #f string is used to format the output.
+        print(timer, end='\r')
+        #end='\r' is used to print the output in the same line.
+        time.sleep(1)
+        #time.sleep function is used to pause the program for 1 second.
+        n -= 1
+        #n is decremented by 1.
+    print('\n')
+    print('Time is up!')
+    #printing the message when time is up.
+
+n = int(input("Enter the time in seconds : "))
+#Taking the time in seconds.
+countdown(n)
+#calling the function countdown and passing the input as n.
